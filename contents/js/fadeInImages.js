1ccd3d13b96dadb28a2a809f5a2eedfdae1596e5
harquail <nook@harquail.com>
2015-06-29 00:25:34 -0400

    closes #29

--- contents/js/fadeInImages.js
+++ contents/js/fadeInImages.js
@@ -1,13 +1,18 @@
-$(document).ready(function() { 
+// Find all the pdf files embedded on a page.
+var imagesToFade = document.getElementsByClassName("img");
 
-    $('img').hide();
-    $('img').each(function(i) {
-        if (this.complete) {
+for (var i = 0; i < imagesToFade.length; i++) {
+  var image = imagesToFade[i];
+  var imageTag = "#" + pdf.id;
+  $(imageTag).hide()
+};
+
+$('img').each(function(i) {
+    if (this.complete) {
+        $(this).fadeIn(500);
+    } else {
+        $(this).load(function() {
             $(this).fadeIn(500);
-        } else {
-            $(this).load(function() {
-                $(this).fadeIn(500);
-            });
-        }
-    });
+        });
+    }
 });
