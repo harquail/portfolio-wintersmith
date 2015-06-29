2cf974a10f0b5d1a5b786ea1b13e685d15093940
harquail <nook@harquail.com>
2015-06-28 23:40:56 -0400

    removed unsightly grey flash

--- contents/js/pdfEmbed.js
+++ contents/js/pdfEmbed.js
@@ -5,9 +5,10 @@ console.log('reached');
 for (var i = 0; i < pdfsToEmbed.length; i++) {
   var pdf = pdfsToEmbed[i];
   var pdfIdTag = "#" + pdf.id;
+  $(pdfIdTag).hide()
   $(pdfIdTag).html("<iframe src='"+pdf.id+".pdf.html' id='"+pdf.id+"'>< iframe>"); 
-  
   $('iframe'+pdfIdTag).load( function() {
+      $(pdfIdTag).fadeIn()
       var printme =  $('iframe'+pdfIdTag).contents().find("head").append("<style type='text/css'>  #page-container{background-image:none; background-color:#fff;} .pf{box-shadow:none;}  </style>");
       console.log(printme);
   });
