8ea2390cb1a8b690ba54e7a692da9257b0c1f356
harquail <nook@harquail.com>
2015-06-28 23:45:56 -0400

    fade in images?

--- contents/js/fadeInImages.js
+++ contents/js/fadeInImages.js
@@ -1,20 +1,13 @@
-// Find all the YouTube video embedded on a page
-var videos = document.getElementsByClassName("youtube");
-console.log('reachedYoutube');
+$(document).ready(function() { 
 
-for (var i = 0; i < videos.length; i++) {
-  var youtube = videos[i];
-
-  // Create an iFrame with autoplay set to true
-  var iframe = document.createElement("iframe");
-  iframe.setAttribute("src",
-    "https://www.youtube.com/embed/" + youtube.id + "?autoplay=0&autohide=1&border=0&wmode=opaque&enablejsapi=1&rel=0");
-
-  // The height and width of the iFrame should be the same as parent
-  iframe.style.width = "720px";
-  iframe.style.height = "405px";
-  iframe.setAttribute("frameborder","0");
-  iframe.setAttribute("allowfullscreen","");
-  youtube.appendChild(iframe);
-
-};
+    $('img').hide();
+    $('img').each(function(i) {
+        if (this.complete) {
+            $(this).fadeIn(500);
+        } else {
+            $(this).load(function() {
+                $(this).fadeIn(500);
+            });
+        }
+    });
+});
