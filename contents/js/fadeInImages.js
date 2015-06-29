caaef7683a4d77af7e7681d6e446be90bae1368a
harquail <nook@harquail.com>
2015-06-29 01:52:44 -0400

    removed image fade which doesn't work anyway

--- contents/js/fadeInImages.js
+++ contents/js/fadeInImages.js
@@ -1,19 +1,15 @@
 // Find all the pdf files embedded on a page.
-var imagesToFade = document.images
-console.log(imagesToFade)
+var imagesToFade = $(".content img")
+// console.log(imagesToFade);
 for (var i = 0; i < imagesToFade.length; i++) {
   var image = imagesToFade[i];
   var imageTag = "#" + image.id;
-  console.log(imageTag)
-  $(image).hide()
-};
+  // console.log(image);
+  // $(image).hide();
+  $(image).addClass('scene_element');
+  $(image).addClass('scene_element--fadeinright');
+  // scene_element
+  // image.
+  console.log($(image));
 
-$('img').each(function(i) {
-    if (this.complete) {
-        $(this).fadeIn();
-    } else {
-        $(this).load(function() {
-            $(this).fadeIn();
-        });
-    }
-});
+};
