9996f51d8a531088999407ab0d2f40c44efdefe3
harquail <nook@harquail.com>
2015-06-29 00:32:05 -0400

    fade images??

--- contents/js/fadeInImages.js
+++ contents/js/fadeInImages.js
@@ -1,18 +1,19 @@
 // Find all the pdf files embedded on a page.
-var imagesToFade = document.getElementsByClassName("img");
-
+var imagesToFade = document.images
+console.log(imagesToFade)
 for (var i = 0; i < imagesToFade.length; i++) {
   var image = imagesToFade[i];
-  var imageTag = "#" + pdf.id;
-  $(imageTag).hide()
+  var imageTag = "#" + image.id;
+  console.log(imageTag)
+  $(image).hide()
 };
 
 $('img').each(function(i) {
     if (this.complete) {
-        $(this).fadeIn(500);
+        $(this).fadeIn();
     } else {
         $(this).load(function() {
-            $(this).fadeIn(500);
+            $(this).fadeIn();
         });
     }
 });
