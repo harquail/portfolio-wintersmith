c9ffbbeb5f4d7f9447cf00e5a53fa9334f3d41bd
harquail <nook@harquail.com>
2015-06-30 15:26:55 -0400

    fixed gray flash for real

--- contents/js/fadeInImages.js
+++ contents/js/fadeInImages.js
@@ -1,15 +1,15 @@
-// Find all the pdf files embedded on a page.
-var imagesToFade = $(".content img")
-// console.log(imagesToFade);
-for (var i = 0; i < imagesToFade.length; i++) {
-  var image = imagesToFade[i];
-  var imageTag = "#" + image.id;
-  // console.log(image);
-  // $(image).hide();
-  $(image).addClass('scene_element');
-  $(image).addClass('scene_element--fadeinright');
-  // scene_element
-  // image.
-  console.log($(image));
-
-};
+// // Find all the pdf files embedded on a page.
+// var imagesToFade = $(".content img")
+// // console.log(imagesToFade);
+// for (var i = 0; i < imagesToFade.length; i++) {
+//   var image = imagesToFade[i];
+//   var imageTag = "#" + image.id;
+//   // console.log(image);
+//   // $(image).hide();
+//   $(image).addClass('scene_element');
+//   $(image).addClass('scene_element--fadeinright');
+//   // scene_element
+//   // image.
+//   console.log($(image));
+//
+// };
--- contents/js/pdfEmbed.js
+++ contents/js/pdfEmbed.js
@@ -1,3 +1,4 @@
+// $( document ).ready(function() {
 // Find all the pdf files embedded on a page.
 var pdfsToEmbed = document.getElementsByClassName("pdfEmbed");
 console.log('reached');
@@ -5,12 +6,12 @@ console.log('reached');
 for (var i = 0; i < pdfsToEmbed.length; i++) {
   var pdf = pdfsToEmbed[i];
   var pdfIdTag = "#" + pdf.id;
-  $(pdfIdTag).hide()
+  $(pdfIdTag).css({ opacity: 0.0 });
   $(pdfIdTag).html("<iframe src='"+pdf.id+".pdf.html' id='"+pdf.id+"'>< iframe>"); 
   $('iframe'+pdfIdTag).load( function() {
-      $(pdfIdTag).fadeIn()
+      $(pdfIdTag).css({ opacity: 1.0 });
       var printme =  $('iframe'+pdfIdTag).contents().find("head").append("<style type='text/css'>  #page-container{background-image:none; background-color:#fff;} .pf{box-shadow:none;}  </style>");
       console.log(printme);
   });
 };
-
+// });
